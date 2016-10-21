2b011564ec089c5802f7ce942bb7dec7e6094514
Artem Nedodaev <anedod@gmail.com>
2016-10-21 22:33:57 +0300

    Third Commit - adding lorem text to homepage

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php $pageTitle = "Bob's Auto Parts - Home Page"; ?>
 <?php include('includes/head.inc'); ?>
 
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Lorem ipsum dolor sit amet</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolores vitae, dolorem voluptatibus porro delectus in quaerat accusamus nostrum nam maxime quia ea nesciunt sunt eius excepturi consectetur blanditiis ex.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolores vitae, dolorem voluptatibus porro delectus in quaerat accusamus nostrum nam maxime quia ea nesciunt sunt eius excepturi consectetur blanditiis ex.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolores vitae, dolorem voluptatibus porro delectus in quaerat accusamus nostrum nam maxime quia ea nesciunt sunt eius excepturi consectetur blanditiis ex.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolores vitae, dolorem voluptatibus porro delectus in quaerat accusamus nostrum nam maxime quia ea nesciunt sunt eius excepturi consectetur blanditiis ex.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo dicta animi omnis. Aliquid mollitia magni incidunt maiores aut libero ipsa tempora, ipsam, dolore suscipit, ut quas adipisci labore fuga quos. ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eveniet, dolorem, veniam placeat odit at numquam obcaecati dolores, aut necessitatibus dignissimos earum quisquam ab expedita. Cupiditate quas illum ex quasi.</p>
+		</div>
+	</div>
+</div>
+
 <?php include('includes/foot.inc'); ?>
