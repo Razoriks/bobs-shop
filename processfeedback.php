89b7766edf60e7767cdab51a472a40726a47a565
Artem Nedodaev <anedod@gmail.com>
2016-10-22 14:11:47 +0300

    Chapter 4 complete Regular expresion and string functions

--- processfeedback.php
+++ processfeedback.php
@@ -2,12 +2,28 @@
 <?php include('includes/head.inc'); ?>
 
 <?php
+
+	$email = trim($_POST['email']);
+	$comment = trim($_POST['comment']);
 	
-	$toaddress = "anedod@gmail.com";
+	if( !eregi("^[a-zA-Z0-9_]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$", $email)){
+		echo "That is not a valid email address. Please return to the previous page and try again.";
+		exit();
+	}
+
+	$toaddress = "anedod@gmail.com"; //Default value
+
+	if(eregi("shop|customer service|retail", $comment))
+		$toaddress = "retail@bobsdomain.com";
+	elseif(eregi("deliver.*|fulfil.*", $comment))
+		$toaddress = "fulfilment@bobsdomain.com";
+	elseif(eregi("bill|account", $comment))
+		$toaddress = "accounts@bobsdomain.com"	;
+
 	$subject = "Feedback from Bob's Shop";
 	$mailcontent = "Customer name: ".trim($_POST['name'])."\n".
-					"Customer email: ".trim($_POST['email'])."\n".
-					"Customer comments: ".trim($_POST['comment']);
+					"Customer email: ".$email."\n".
+					"Customer comments: ".$comment;
 	$fromaddress = "info@anwes.ru";
 
 	mail($toaddress, $subject, $mailcontent, $fromaddress);
