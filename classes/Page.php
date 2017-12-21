58540316ed12ab5d9fed8be9bde20295ad83bf5e
Artem Nedodaev <anedod@gmail.com>
2017-12-21 15:39:36 +0300

    Chapter 6 Completed

--- classes/Page.php
+++ classes/Page.php
@@ -3,9 +3,38 @@
 * 
 */
 class Page {
+
+	var $content;
+	var $title = "TLA Consalting Pty Ltd";
+	var $keywords = "TLA Consalting";
+
+	var $buttons = array(
+							'Home' => 'home.php', 
+							'Contact' => 'contact.php', 
+							'Services' => 'services.php', 
+							'Site map' => 'map.php', 
+						);
 	
-	function __construct(argument)
-	{
-		# code...
+	function setContent($newcontent) {
+		$this->content = $newcontent;
 	}
+
+	function setTitle($newtitle) {
+		$this->title = $newtitle;
+	}
+
+	function setKeywords($newkeywords) {
+		$this->keywords = $newkeywords;
+	}
+
+	function setButtons($newbuttons) {
+		$this->buttons = $newbuttons;
+	}
+
+	function display() {
+		include("../includes/head.inc");
+		echo $this->content;
+		include("../includes/foot.inc");
+	}
+
 }
